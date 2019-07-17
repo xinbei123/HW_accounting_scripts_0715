404a98a2959bee4962da253352c723acf01ce96c
Aileen Liu <aileenliu184@gmail.com>
2019-07-16 18:51:34 -0700

    original files

---
+++ melons.py
@@ -0,0 +1,38 @@
+melons = {
+    'Honeydew': {
+        'price': 0.99,
+        'seedlessness': True,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+    'Crenshaw': {
+        'price': 2.00,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+    'Crane': {
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+    'Casaba': {
+        'price': 2.50,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    },
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedlessness': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
+    }
+
+}
