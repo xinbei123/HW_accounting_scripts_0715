36e3611b7391615819fb95865bfdf81eae6901ac
Aileen Liu <aileenliu184@gmail.com>
2019-07-16 18:57:55 -0700

    add new function

--- melon_info.py
+++ melon_info.py
@@ -1,25 +1,32 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+# from melons import melon_names, melon_seedlessness, melon_prices
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
+# def print_melon(name, seedless, price):
+#     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
 
+from melons import melons
 
+def print_all_melons(melons):
+    for melon_names, attributes in melons.items():
+        print(melon_names.upper())
+
+        for attribute, value in attributes.items():
+            print (f"{attribute} : {value}")
+
+print(print_all_melons(melons))
 
 
 
